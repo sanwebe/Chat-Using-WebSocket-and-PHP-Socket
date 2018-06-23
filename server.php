8e9f5310a34954b9e41466b4cb385e73a762926b
Sanwebe <saaraan@gmail.com>
2018-06-23 16:00:08 +0530

    Update server.php

--- server.php
+++ server.php
@@ -49,7 +49,7 @@ while (true) {
 		{
 			$received_text = unmask($buf); //unmask data
 			$tst_msg = json_decode($received_text, true); //json decode 
-			$user_name = $tst_msg['name'] //sender name
+			$user_name = $tst_msg['name']; //sender name
 			$user_message = $tst_msg['message']; //message text
 			$user_color = $tst_msg['color']; //color
 			
